0be836778e3f908fd3f5107023174744726e5a2a
Mr.Gazi <santo@gaziii.local>
2022-05-08 04:01:28 +0600

    A simple java program has been added

---
+++ MyFancyJavaProject.java
@@ -0,0 +1,10 @@
+public class MyFancyJavaProject {
+	public static void addFunction(int a, int b){
+		System.out.print("Result" + a+b);
+	}
+
+	public static void main(String[] args){
+		System.out.print("Hello World");
+		addFunction(5,8);
+	}
+}
