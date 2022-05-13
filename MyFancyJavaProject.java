dc40c10176cdd8ddff3bb8ec3a9b0d85a04ab568
Mr.Gazi <santo@gaziii.local>
2022-05-13 19:45:05 +0600

    slight change in java program

--- MyFancyJavaProject.java
+++ MyFancyJavaProject.java
@@ -1,10 +1,5 @@
 public class MyFancyJavaProject {
-	public static void addFunction(int a, int b){
-		System.out.print("Result" + a+b);
-	}
-
 	public static void main(String[] args){
-		System.out.print("Hello World");
-		addFunction(5,8);
+		System.out.print("Hello World Changed");
 	}
 }
